0f5cff89b790630d177c35495e45abc4d22912b4
saifsammy <saifsammy653@gmail.com>
2023-05-05 21:50:20 +0600

    remove unnessasary code from chef recipes

--- src/Pages/Home/ChefRecipes/ChefRecipes.jsx
+++ src/Pages/Home/ChefRecipes/ChefRecipes.jsx
@@ -21,15 +21,6 @@ const ChefRecipes = () => {
                         <p>"{bio}"</p>
                     </div>
                 </a>
-                {/* <a rel="noopener noreferrer" href="#" className="block max-w-sm gap-3 mx-auto sm:max-w-full group hover:no-underline focus:no-underline lg:grid lg:grid-cols-12 dark:bg-gray-900">
-                    <img src={chefsPicture} alt="" className="object-cover w-full h-64 rounded sm:h-96 lg:col-span-7 dark:bg-gray-500" />
-                    <div className="p-6 space-y-2 lg:col-span-5 banner-text">
-                        <h1 className="text-2xl font-semibold sm:text-4xl group-hover:underline group-focus:underline">Hello Myself {chefsName}</h1>
-                        <span className="text-xs dark:text-gray-400">I am from {country}</span>
-                        <h3>I have {years_experience} years of experience. I know more than {recipes.length} recipes & almost {likes} peoples likes my dishes</h3>
-                        <h6>"{bio}"</h6>
-                    </div>
-                </a> */}
                 <div className="grid justify-center grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3">
                     {
                         recipes.map((recipe, i) => <Recipes
