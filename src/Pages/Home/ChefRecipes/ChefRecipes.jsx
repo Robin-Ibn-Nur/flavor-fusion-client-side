e3c5c9603745ea6a368ea791dcd1084e5b0cde15
saifsammy <saifsammy653@gmail.com>
2023-05-05 21:48:59 +0600

    chef's profile updated successFully

--- src/Pages/Home/ChefRecipes/ChefRecipes.jsx
+++ src/Pages/Home/ChefRecipes/ChefRecipes.jsx
@@ -13,16 +13,23 @@ const ChefRecipes = () => {
         <section className="text-white">
             <div className="container max-w-6xl p-6 mx-auto space-y-6 sm:space-y-12">
                 <a rel="noopener noreferrer" href="#" className="block max-w-sm gap-3 mx-auto sm:max-w-full group hover:no-underline focus:no-underline lg:grid lg:grid-cols-12 dark:bg-gray-900">
-                    <LazyLoad height={200} once>
-                        <img src={chefsPicture} alt="" className="object-cover w-full h-64 rounded sm:h-96 lg:col-span-7 dark:bg-gray-500" />
-                    </LazyLoad>
+                    <img src={chefsPicture} alt="" className="object-cover w-full h-64 rounded sm:h-96 lg:col-span-7 dark:bg-gray-500" />
+                    <div className="p-6 space-y-2 lg:col-span-5">
+                        <h3 className="text-2xl font-semibold sm:text-4xl group-hover:underline group-focus:underline">Hello Myself {chefsName}</h3>
+                        <span className="text-xs dark:text-gray-400">I am from {country}</span>
+                        <p>I have {years_experience} years of experience. I know more than {recipes.length} recipes & almost {likes} peoples likes my dishes</p>
+                        <p>"{bio}"</p>
+                    </div>
+                </a>
+                {/* <a rel="noopener noreferrer" href="#" className="block max-w-sm gap-3 mx-auto sm:max-w-full group hover:no-underline focus:no-underline lg:grid lg:grid-cols-12 dark:bg-gray-900">
+                    <img src={chefsPicture} alt="" className="object-cover w-full h-64 rounded sm:h-96 lg:col-span-7 dark:bg-gray-500" />
                     <div className="p-6 space-y-2 lg:col-span-5 banner-text">
                         <h1 className="text-2xl font-semibold sm:text-4xl group-hover:underline group-focus:underline">Hello Myself {chefsName}</h1>
                         <span className="text-xs dark:text-gray-400">I am from {country}</span>
                         <h3>I have {years_experience} years of experience. I know more than {recipes.length} recipes & almost {likes} peoples likes my dishes</h3>
                         <h6>"{bio}"</h6>
                     </div>
-                </a>
+                </a> */}
                 <div className="grid justify-center grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3">
                     {
                         recipes.map((recipe, i) => <Recipes
