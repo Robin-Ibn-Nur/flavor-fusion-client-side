f0bda03d63782a36027c10040bc880f4f6540265
saifsammy <saifsammy653@gmail.com>
2023-05-05 21:51:31 +0600

    lazy load added on chef recipes

--- src/Pages/Home/ChefRecipes/ChefRecipes.jsx
+++ src/Pages/Home/ChefRecipes/ChefRecipes.jsx
@@ -10,27 +10,29 @@ const ChefRecipes = () => {
     const { id, chefsName, chefsPicture, country, bio, likes, recipes, years_experience } = data;
 
     return (
-        <section className="text-white">
-            <div className="container max-w-6xl p-6 mx-auto space-y-6 sm:space-y-12">
-                <a rel="noopener noreferrer" href="#" className="block max-w-sm gap-3 mx-auto sm:max-w-full group hover:no-underline focus:no-underline lg:grid lg:grid-cols-12 dark:bg-gray-900">
-                    <img src={chefsPicture} alt="" className="object-cover w-full h-64 rounded sm:h-96 lg:col-span-7 dark:bg-gray-500" />
-                    <div className="p-6 space-y-2 lg:col-span-5">
-                        <h3 className="text-2xl font-semibold sm:text-4xl group-hover:underline group-focus:underline">Hello Myself {chefsName}</h3>
-                        <span className="text-xs dark:text-gray-400">I am from {country}</span>
-                        <p>I have {years_experience} years of experience. I know more than {recipes.length} recipes & almost {likes} peoples likes my dishes</p>
-                        <p>"{bio}"</p>
+        <LazyLoad>
+            <section className="text-white">
+                <div className="container max-w-6xl p-6 mx-auto space-y-6 sm:space-y-12">
+                    <a rel="noopener noreferrer" href="#" className="block max-w-sm gap-3 mx-auto sm:max-w-full group hover:no-underline focus:no-underline lg:grid lg:grid-cols-12 dark:bg-gray-900">
+                        <img src={chefsPicture} alt="" className="object-cover w-full h-64 rounded sm:h-96 lg:col-span-7 dark:bg-gray-500" />
+                        <div className="p-6 space-y-2 lg:col-span-5">
+                            <h3 className="text-2xl font-semibold sm:text-4xl group-hover:underline group-focus:underline">Hello Myself {chefsName}</h3>
+                            <span className="text-xs dark:text-gray-400">I am from {country}</span>
+                            <p>I have {years_experience} years of experience. I know more than {recipes.length} recipes & almost {likes} peoples likes my dishes</p>
+                            <p>"{bio}"</p>
+                        </div>
+                    </a>
+                    <div className="grid justify-center grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3">
+                        {
+                            recipes.map((recipe, i) => <Recipes
+                                key={i}
+                                recipe={recipe}
+                            ></Recipes>)
+                        }
                     </div>
-                </a>
-                <div className="grid justify-center grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3">
-                    {
-                        recipes.map((recipe, i) => <Recipes
-                            key={i}
-                            recipe={recipe}
-                        ></Recipes>)
-                    }
                 </div>
-            </div>
-        </section>
+            </section>
+        </LazyLoad>
 
     );
 };
